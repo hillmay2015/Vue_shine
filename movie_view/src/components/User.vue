c20b49e2b6964977cc28641ae6d9e5536847e828
xym <727347070@qq.com>
2020-02-07 21:30:36 +0800

    vue 初始化

---
+++ movie_view/src/components/User.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>user page</h2>
+    <ul>
+      <li>
+       苹果
+      </li>
+      <li>
+       香蕉
+      </li>
+      <li>
+       栗子
+      </li>
+      <li>
+      菠萝
+      </li>
+      <br>
+      <li>
+      榴莲
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+      杨桃
+      </li>
+      <li>
+       葡萄
+      </li>
+      <li>
+       西瓜
+      </li>
+      <li>
+       荔枝
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'this is user page'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ movie_view/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import User from '@/components/User'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+      {
+          path: '/User',
+          name: 'hello xym 2020.2.7',
+          component: User
+      }
+  ]
+})
