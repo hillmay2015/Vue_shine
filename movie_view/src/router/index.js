ee38dba18a1b4f9e4d840aa900ce8ae6432071af
xym <727347070@qq.com>
2020-02-08 21:22:33 +0800

    重新写一个页面来

--- movie_view/src/router/index.js
+++ movie_view/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import User from '@/components/User'
+import Index from '@/components/Index'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
           path: '/User',
           name: 'hello xym 2020.2.7',
           component: User
+      },
+      {
+          path: '/Index',
+          name: 'Index',
+          component: Index
       }
   ]
 })
